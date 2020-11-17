fbbd0727e4cd25454af1a88ed9625e134012bf77
Parker Stanley <parker.stanley@SOMO-M-DC-005.fios-router.home>
2020-11-17 08:56:43 -0500

    completed 15.4 practice

--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodingEventsDemo.ViewModels
 {
     public class AddEventViewModel
     {
+        [Required(ErrorMessage = "Name is a required field.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is a required field.")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
         public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
     }
 }
