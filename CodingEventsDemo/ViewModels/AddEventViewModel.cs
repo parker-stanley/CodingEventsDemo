5a31b86a3c869d6e44951e795076f860b1d9dd01
Parker Stanley <parker.stanley@SOMO-M-DC-005.fios-router.home>
2020-11-17 08:24:58 -0500

    completed first walkthrough with the video from chapter 15 reading

---
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
