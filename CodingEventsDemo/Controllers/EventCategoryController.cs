07a38491b99036edc96911327ad2874f78351a2c
Parker Stanley <parker.stanley@somoglobal.com>
2020-12-03 21:38:42 -0600

    completed exercises however I noticed when testing the application hangs when trying to load /eventcategory because there is no data in the list yet. Need to double check my condition checking for this and see why it's not triggering

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        //GET
+        public IActionResult Index()
+        {
+            List<EventCategory> eventCategories = context.EventCategories.ToList();
+
+            return View();
+        }
+    }
+}
--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -7,9 +7,12 @@ namespace CodingEventsDemo.Data
     public class EventDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
         {
         }
+
+
     }
 }
